64800aa1c1d81684b3386377e95df547dad2cdcb
aashishkar <aashishkarmacharya79@gmail.com>
2024-12-13 14:21:46 +0545

    mainedit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:sprint1/view/onboarding_view.dart';
-import 'package:sprint1/view/login_view.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: OnboardingView(),
-    routes: {
-      '/login': (context) => LoginView(),
-    },
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+      home: OnboardingView(),
+    );
+  }
 }
